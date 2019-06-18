4b695e7762093b4bf67d4c8297eb7c804f4ba93c
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2019-06-19 00:40:40 +0200

    Envio doc desde morse

--- prueba.2.py
+++ prueba.2.py
@@ -1,12 +1,12 @@
-import morse
+from morse import *
 import time
 from docx import Document
 
 mensaje = input("Dime algo: ")
 
-telegrama = morse.toMorse(mensaje)
+telegrama = toMorse(mensaje)
 print(telegrama)
-original = morse.toPlain(telegrama)
+original = toPlain(telegrama)
 print(original)
 
 print(time.strftime("%d/%m/%Y", time.gmtime()))
